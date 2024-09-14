6c968c4378d1fd578ae21b0986cfda2265f31bf6
Ajith-M-001 <ajithguru741@gmail.com>
2024-09-14 22:43:29 +0530

    connecting to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import connectDB from "./database/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +13,16 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("error connecting to mongodb ", error);
+  }
+};
+
+startServer();
