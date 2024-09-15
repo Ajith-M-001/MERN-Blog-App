b7517f8e20661531d3b9e6dda4d81bf7b821c377
Ajith-M-001 <ajithguru741@gmail.com>
2024-09-16 04:41:08 +0530

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -37,6 +38,38 @@ export const signUp = async (req, res, next) => {
   }
 };
 
-export const signIn = (req, res) => {
-  console.log("req", req.body);
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(errorHandler(400, "Required fields can't be empty"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(400, "email or password incorrect"));
+    }
+
+    const verifyPassword = await bcryptjs.compare(password, validUser.password);
+    if (!verifyPassword) {
+      return next(errorHandler(400, "email or password incorrect"));
+    }
+
+    const token = jwt.sign(
+      {
+        userId: validUser._id,
+      },
+      process.env.JWTSECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json({ user: rest });
+    console.log("valid123", rest);
+  } catch (error) {
+    next(error);
+  }
 };
