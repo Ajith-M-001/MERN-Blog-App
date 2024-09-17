c76519f389fee21bfd515aea8e4935b94d85a0d5
Ajith-M-001 <ajithguru741@gmail.com>
2024-09-18 05:01:15 +0530

    sidebar implemenatation in dashboard

---
+++ client/src/components/DashSideBar.jsx
@@ -0,0 +1,44 @@
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiUser } from "react-icons/hi";
+import { HiMiniArrowRightStartOnRectangle } from "react-icons/hi2";
+import { Link, useLocation } from "react-router-dom";
+
+const DashSideBar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get("tab");
+    if (tabFromURL) {
+      setTab(tabFromURL);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label="User"
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            icon={HiMiniArrowRightStartOnRectangle}
+            className="cursor-pointer"
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSideBar;
